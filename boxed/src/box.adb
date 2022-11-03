2c5412a89bad9da0b0c0eedc8fd336f69a273d18
R Srinivasan <rs@toprllc.com>
2022-11-03 05:07:50 -0400

    complete find_words

--- boxed/src/box.adb
+++ boxed/src/box.adb
@@ -1,7 +1,7 @@
 with Ada.Characters.Handling; use Ada.Characters.Handling ;
 with Ada.Text_Io; use Ada.Text_Io;
 with Ada.Integer_Text_Io; Use Ada.Integer_Text_Io;
-
+with GNAT.Source_Info ;
 with dictionary ;
 package body box is
 
@@ -9,6 +9,7 @@ package body box is
 
    procedure Initialize is
    begin
+      Put(GNAT.Source_Info.Source_Location); Put(" "); Put_Line(GNAT.Source_Info.Enclosing_Entity);
       fulldict := dictionary.Load ("etc/spellcheck-dict.txt") ;
    end Initialize ;
 
@@ -65,8 +66,10 @@ package body box is
    end Show ;
 
    procedure Solve( g : Game ) is
+      w : WordsType ;
    begin
-      null ;
+      Put(GNAT.Source_Info.Source_Location); Put(" "); Put_Line(GNAT.Source_Info.Enclosing_Entity);
+      w := FindWords(g) ;
    end Solve ;
 
    function Get( g : Game ; s : Side ; p : Position ) return Character is
@@ -74,4 +77,77 @@ package body box is
       return g(s,p) ;
    end Get ;
   
+   function FindWords( g : Game ) return WordsType is
+      result : WordsType ;
+   begin
+      Put(GNAT.Source_Info.Source_Location); Put(" "); Put_Line(GNAT.Source_Info.Enclosing_Entity);
+      for s in Side'Range
+      loop
+         for p in Position'Range
+         loop
+            result(s,p) := Findwords( g , s , p );
+         end loop ;
+      end loop ;
+      return result ;
+   end FindWords ;
+   procedure Show( w : WordsType ) is
+   begin
+      for s in Side'Range
+      loop
+         for p in Position'Range
+         loop
+            Put("Words Starting with ");
+            Put( To_String(Words_Pkg.Element(Words_Pkg.First(w(s,p))))'First ) ;
+            New_Line;
+            Show( w(s,p) );
+         end loop ;
+      end loop ;
+   end Show ;
+
+   procedure Addwords( v : in out Words_Pkg.Vector ; g : game ; s : side ; p : Position ; starter : String ) is
+   begin
+      --Put(GNAT.Source_Info.Source_Location); Put(" "); Put_Line(GNAT.Source_Info.Enclosing_Entity);
+      for ss in side'Range
+      loop
+         if ss /= s
+         then 
+            for pp in Position'Range
+            loop
+               declare
+                  xpword : String := starter & g(ss,pp) ;
+               begin
+                  --Put_Line(xpword);
+                  if xpword'Length >= 3 and then dictionary.IsWord(fulldict,xpword)
+                  then
+                     Words_Pkg.Append(v , To_Unbounded_String(xpword)) ;
+                     Put_Line(xpword);
+                  end if ;
+                  if xpword'Length <= 6
+                  then
+                     AddWords( v , g , ss , pp , xpword );
+                  end if ;
+               end;
+            end loop ;
+         end if ;
+      end loop ;
+   end AddWords ;
+
+   function Findwords( g : Game ; s : Side ; p : Position ) return Words_Pkg.Vector is
+      result : Words_Pkg.Vector;
+      wstart : String (1..1) := (others => g(s,p)) ;
+   begin
+      Put(GNAT.Source_Info.Source_Location); Put(" "); Put_Line(GNAT.Source_Info.Enclosing_Entity);
+      AddWords( result , g , s , p , wstart );
+      return result ;
+   end FindWords ;
+
+   procedure Show( p : Words_Pkg.Cursor ) is
+   begin
+       Put_Line( To_String(Words_Pkg.Element(p)) ); 
+   end Show ;
+   procedure Show( wv : Words_Pkg.Vector ) is
+   begin
+       Words_Pkg.Iterate( wv , Show'Access );
+   end Show ;
+
 end box ;
--- boxed/src/box.ads
+++ boxed/src/box.ads
@@ -12,14 +12,27 @@ package box is
 
       type Side is
       ( left, top, right, bottom ) ;
-      
+
       LETTERS_PER_SIDE : constant := 3 ;
       type Position is new integer range 1..LETTERS_PER_SIDE ;
 
       type Game is array (Side'Range , Position'Range ) of Character ;
       function Create( arg : string ) return Game with
          Precondition => ( arg'Length = Game'Length(1) * Game'Length(2) );
+      
+      package Words_Pkg is new Ada.Containers.Vectors( 
+         Natural ,
+         Ada.Strings.Unbounded.Unbounded_String );
+
+      type WordsType is array (Side'Range , Position'Range ) of Words_Pkg.Vector ;
+
       procedure Show( g : Game ) ;
       procedure Solve( g : Game ) ;
 
+      function FindWords( g : Game ) return wordsType ;
+      procedure Show( w : WordsType );
+
+      function Findwords( g : Game ; s : Side ; p : Position ) return Words_Pkg.Vector ;
+      procedure Show( wv : Words_Pkg.Vector );
+   
 end box ;
